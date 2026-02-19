eceeac380657b33f64a037d2308f8d48c3fc2d85
HUZAIFA ANSARI <155826925+huzaifa-ansari-tech@users.noreply.github.com>
2026-02-19 18:25:33 +0000

    Variables Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const username = "huzaifa";
+let fullname = "HUZAIFA ANSARI"
+var password = "12345";
+id = 11224455
+
+// username = "async";
+fullname = "asyncronous";
+password = 6789;
+
+console.table([username, fullname, password, id]);
