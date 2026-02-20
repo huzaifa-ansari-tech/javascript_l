4a1492edafc5d5b741665ad023ce0cc17e536401
HUZAIFA ANSARI <155826925+huzaifa-ansari-tech@users.noreply.github.com>
2026-02-20 13:56:25 +0000

    Datatype Conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+console.log("Working...");
+
+// alert(3+3); // We are using nodejs not the browser
+// Code must be readable
+
+let name = "huzaifa"; // String
+let age = 17; // Number
+let isLoggedIn = false; // Boolean
+
+// symbol => Unique
+// null => Standalone
+// undefined 
+
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof isLoggedIn);
+
+const num = Number(name);
+// Give value NaN and the type is "number"
+
+const ageInStr = String(age);
+// Give value in str    
+
+
