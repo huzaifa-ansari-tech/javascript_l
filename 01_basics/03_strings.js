b540aa25e30ab3be47c0f3e992c4a1cd9b2afdec
HUZAIFA ANSARI <155826925+huzaifa-ansari-tech@users.noreply.github.com>
2026-02-21 13:05:10 +0000

    String Methods/Properties

---
+++ 01_basics/03_strings.js
@@ -0,0 +1,12 @@
+let username = "huzaifa-ansari-com"
+let fullname = "     huzaifa      "
+console.log(username.length);
+console.log(username.charAt(3));
+console.log(username.indexOf("a"));
+console.log(username.toUpperCase());
+
+console.log(username.substring(2, 8));
+console.log(username.slice(-10, -3)); // -10 not included and -3 included
+console.log(username.split(''));
+console.log(fullname.replaceAll(" ", "_"));
+
